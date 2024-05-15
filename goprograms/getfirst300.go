d50ce6a82992793e4dc3a7a0ff29d59975ea5c46
aalfaro5 <aalfaro5@mail.csuchico.edu>
2024-05-15 23:49:58 +0000

    got the first 300 hostnames in large.txt and use that for DNS programs

---
+++ goprograms/getfirst300.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    // Open the input file
+    inputFile, err := os.Open("input/large.txt")
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    defer inputFile.Close()
+
+    // Create a new output file
+    outputFile, err := os.Create("output.txt")
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+    defer outputFile.Close()
+
+    // Create a scanner to read from the input file
+    scanner := bufio.NewScanner(inputFile)
+
+    // Write the first 300 lines into the output file
+    lineCount := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+        _, err := fmt.Fprintln(outputFile, line)
+        if err != nil {
+            fmt.Println("Error:", err)
+            return
+        }
+        lineCount++
+        if lineCount >= 300 {
+            break
+        }
+    }
+
+    // Check for errors during scanning
+    if err := scanner.Err(); err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+    fmt.Println("First 300 lines copied to output.txt")
+}
+
